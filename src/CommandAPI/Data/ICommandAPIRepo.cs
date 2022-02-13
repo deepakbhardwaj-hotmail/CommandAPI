3b7d7a4f447bd67048794a8d645b9d0dc1f3696c
Deepak Bhardwaj <deepakbhardwaj@hotmail.com>
2022-02-14 01:17:47 +0530

    Create Repository Interface

---
+++ src/CommandAPI/Data/ICommandAPIRepo.cs
@@ -0,0 +1,14 @@
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public interface ICommandAPIRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand();
+        void DeleteCommand();
+        void UpdateCommand();
+    }
+}
