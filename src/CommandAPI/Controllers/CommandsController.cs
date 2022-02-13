0eb7dbfd314a511cfa3ffdbe327274125b53085b
Deepak Bhardwaj <deepakbhardwaj@hotmail.com>
2022-02-14 00:19:42 +0530

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class CommandsController:ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() 
+        {
+            return new string[] {"this", "is", "hard", "coded"};
+        }
+    }
+}
